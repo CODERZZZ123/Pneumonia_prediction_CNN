399a9b59c8ea876f7aff40b3b6b4020656d95310
Saurabh Patel <999.sbpatel@gmail.com>
2023-12-29 23:22:53 +0530

    unecceasry code removed

--- resnets_utils.py
+++ resnets_utils.py
@@ -68,24 +68,6 @@ class BatchNormalization(Layer):
         return self.gamma * x + self.beta
 
 
-def load_dataset():
-    train_dataset = h5py.File('datasets/train_signs.h5', "r")
-    # your train set features
-    train_set_x_orig = np.array(train_dataset["train_set_x"][:])
-    train_set_y_orig = np.array(
-        train_dataset["train_set_y"][:])  # your train set labels
 
-    test_dataset = h5py.File('datasets/test_signs.h5', "r")
-    # your test set features
-    test_set_x_orig = np.array(test_dataset["test_set_x"][:])
-    test_set_y_orig = np.array(
-        test_dataset["test_set_y"][:])  # your test set labels
-
-    classes = np.array(test_dataset["list_classes"][:])  # the list of classes
-
-    train_set_y_orig = train_set_y_orig.reshape((1, train_set_y_orig.shape[0]))
-    test_set_y_orig = test_set_y_orig.reshape((1, test_set_y_orig.shape[0]))
-
-    return train_set_x_orig, train_set_y_orig, test_set_x_orig, test_set_y_orig, classes
 
 
